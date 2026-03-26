88c2f0fabc439e7758d0ea42e58df5aad746214c
ishita <ishimishi726@gmail.com>
2026-03-26 17:46:46 +0530

    updated

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/profile",protect, getProfile);
+router.get("/profile", getProfile);
 router.get("/google",passport.authenticate("google", { scope: ["profile", "email"] }));
 router.get("/google/callback", passport.authenticate("google", { session: false }),googleCallback);
 export default router;
